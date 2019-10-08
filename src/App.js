79be2ccbab1d23cdab7576bb955ee64713d2afab
Mary <mary.clayton1@hotmail.com>
2019-10-08 05:40:56 -0700

    converted props to context api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import axios from 'axios';
+import { ProductContext } from './contexts/ProductContext'
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -13,28 +16,44 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		axios
+		.post(data, item)
+		.then(res => {
+			this.setState({
+				items: res.data
+			})
+			this.props.history.push('/cart');
+		})
+		.catch(err => {
+			console.log(err);
+		})
 	};
 
 	return (
 		<div className="App">
+			<CartContext.Provider value= {cart}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+			<ProductContext.Provider value={{products, addItem}}>
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component = {Products}
+				// render={() => (
+				// 	<Products
+				// 		products={products}
+				// 		addItem={addItem}
+				// 	/>
+				// )}
 			/>
-
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component = {ShoppingCart}
+				// render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
+			</CartContext.Provider>
 		</div>
 	);
 }
