ff1ccab152d9860e75f74064065e4920351d1954
Mary <mary.clayton1@hotmail.com>
2019-10-08 06:44:21 -0700

    implemented add and remove items from cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-import axios from 'axios';
 import { ProductContext } from './contexts/ProductContext'
 
 // Components
@@ -16,44 +15,35 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
-		axios
-		.post(data, item)
-		.then(res => {
-			this.setState({
-				items: res.data
-			})
-			this.props.history.push('/cart');
-		})
-		.catch(err => {
-			console.log(err);
-		})
-	};
-
+		setCart([...cart, item])
+	}
+	const removeItem = itemTitle => {
+		setCart(cart.filter((item => item.title !== itemTitle)
+	))
+	}
+	if (!products) {
+		return <div>loading Shop...</div>
+	}
 	return (
 		<div className="App">
-			<CartContext.Provider value= {cart}>
+			<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value= {{cart, removeItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<ProductContext.Provider value={{products, addItem}}>
+			
 			<Route
 				exact
 				path="/"
 				component = {Products}
-				// render={() => (
-				// 	<Products
-				// 		products={products}
-				// 		addItem={addItem}
-				// 	/>
-				// )}
+				
 			/>
 			<Route
 				path="/cart"
 				component = {ShoppingCart}
-				// render={() => <ShoppingCart cart={cart} />}
 			/>
-			</ProductContext.Provider>
 			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
